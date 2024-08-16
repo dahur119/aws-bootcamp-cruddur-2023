fb29a022640e724f47a16ae2ff7611e58d6de8e4
Adedayo aderoba <dahurdayo@gmail.com>
2024-08-16 00:12:33 +0000

    update the dbs for list and get conversation

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -22,6 +22,8 @@ class Db:
     with open(template_path, 'r') as f:
       template_content = f.read()
     return template_content
+  
+  
 
   def init_pool(self):
     connection_url = os.getenv("CONNECTION_URL")
@@ -35,11 +37,11 @@ class Db:
     for key, value in params.items():
       print(key, ":", value)
 
-  def print_sql(self,title,sql):
+  def print_sql(self,title,sql, params={}):
     cyan = '\033[96m'
     no_color = '\033[0m'
     print(f'{cyan} SQL STATEMENT-[{title}]------{no_color}')
-    print(sql)
+    print(sql, params)
   def query_commit(self,sql,params={}):
     self.print_sql('commit with returning',sql)
 
@@ -57,6 +59,15 @@ class Db:
           return returning_id
     except Exception as err:
       self.print_sql_err(err)
+
+  def query_value(self,sql,params={}):
+      self.print_sql('value',sql,params)
+
+      with self.pool.connection() as conn:
+        with conn.cursor() as cur:
+          cur.execute(sql,params)
+          json = cur.fetchone()
+          return json[0]
   # when we want to return a json object
   def query_array_json(self,sql,params={}):
     self.print_sql('array',sql)
